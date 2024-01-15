f4c000b0d30d254b9fcd0f7658fbe3c60cb4114f
Kamila Stawarska <kamila.stawarska@gmail.com>
2024-01-15 18:27:29 +0100

    Player and player definition scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int Speed { get; private set; }
+    public int Agility { get; private set; }
+    public int Strength { get; private set; }
+    public bool IsGuide { get; set; }
+    
+    [SerializeField] private PlayerDefinition definition;
+    
+    private void Start()
+    {
+        SetRandomValues();
+    }
+
+    private void SetRandomValues()
+    {
+        Speed = Random.Range(definition.MinSpeed, definition.MaxSpeed);
+        Agility = Random.Range(definition.MinAgility, definition.MaxAgility);
+        Strength = Random.Range(definition.MinStrength, definition.MaxStrength);
+        Debug.Log("Speed: " + Speed + ", agility: " + Agility + ", strength: " + Strength);
+    }
+}  
